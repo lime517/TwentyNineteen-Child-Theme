f36761641b051bae29abd58c9ae11072bef1df7c
Joe Greve <joe@elexicon.com>
2019-03-27 15:44:15 -0400

    Fixes

--- functions.php
+++ functions.php
@@ -7,10 +7,10 @@ function enqueue_core_files() {
     wp_enqueue_style('sl-parent-styles', get_template_directory_uri() . '/style.css');
 
     // Child files
-    $maincss_cache_buster = date("YmdHis", filemtime( get_stylesheet_uri() . '/main.css'));
-    $scriptjs_cache_buster = date("YmdHis", filemtime( get_stylesheet_uri() . '/script.js'));
-    wp_enqueue_style( 'sl-main', get_stylesheet_uri() . '/main.css', array(), $maincss_cache_buster, 'all' );
-    wp_enqueue_script( 'sl-script', get_stylesheet_uri() . '/script.js', array('jquery'), $scriptjs_cache_buster );
+    $maincss_cache_buster = date("YmdHis", filemtime( get_stylesheet_directory() . '/main.css'));
+    $scriptjs_cache_buster = date("YmdHis", filemtime( get_stylesheet_directory() . '/script.js'));
+    wp_enqueue_style( 'sl-main', get_stylesheet_directory_uri() . '/main.css', array(), $maincss_cache_buster, 'all' );
+    wp_enqueue_script( 'sl-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'), $scriptjs_cache_buster );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_core_files' );
 
